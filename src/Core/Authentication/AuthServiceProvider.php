61dc709264ea081bc7400176e6e246782300360e
Isaac Dew <lildew2@gmail.com>
2021-12-14 23:53:00 -0700

    Fix to Auth service provider

--- src/Core/Authentication/AuthServiceProvider.php
+++ src/Core/Authentication/AuthServiceProvider.php
@@ -8,7 +8,7 @@ use Overland\Core\Interfaces\ServiceProvider;
 class AuthServiceProvider extends ServiceProvider {
     public function boot() {
         $this->app->singleton('auth', function() {
-            return new Auth();
+            return new \Overland\Core\Authentication\Auth();
         });
 
         Auth::setApp($this->app);
