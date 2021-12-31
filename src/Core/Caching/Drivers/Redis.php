af92046f5e8dea0e828b2e26ed0452e4fe83509e
Isaac Dew <lildew2@gmail.com>
2021-12-31 00:37:50 -0700

    Add support for redis caching

---
+++ src/Core/Caching/Drivers/Redis.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Overland\Core\Caching\Drivers;
+
+use Overland\Core\Caching\CacheDriver;
+use \Predis\Client;
+
+class Redis extends CacheDriver
+{
+    protected $client;
+
+    public function __construct($app)
+    {
+        parent::__construct($app);
+
+        $this->client = new Client(
+            $app->config()->get('app.cache.options')
+        );
+    }
+
+    public function get($key)
+    {
+        return $this->client->get($key);
+    }
+
+    public function has($key)
+    {
+        return $this->client->exists($key);
+    }
+
+    public function put($key, $value, $seconds = 0)
+    {
+        $this->client->set($key, $value);
+
+        if($seconds) {
+            $this->client->expire($key, $seconds);
+        }
+    }
+
+    public function forget($key)
+    {
+        return $this->client->delete($key);
+    }
+}
